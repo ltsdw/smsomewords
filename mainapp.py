54851acfdc5c3f81405230eaba7b049bdfd4700f
ltsdw <alandms13@gmail.com>
2020-07-08 00:19:52 -0300

    first commit

---
+++ mainapp.py
@@ -0,0 +1,9 @@
+#! /bin/python
+
+from mypack.tkwindow import *
+
+if __name__ == '__main__':
+    root = Tk(className='listador')
+    Application(root)
+    root.mainloop()
+
---
+++ mypack/appenditemquit.py
@@ -0,0 +1,42 @@
+#! /hint/python
+
+from os import system, path, environ
+
+def createWriteFile(item):
+    f = open(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt', 'a+')
+    f.write(item + '\n')
+    f.close()
+
+def writeFile(item):
+    f = open(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt', 'a')
+    f.write(item + '\n')
+    f.close()
+
+def checksames(item):
+    try:
+        with open(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt') as f:
+            if item not in f.read() and item != "":
+                return True
+            else:
+                return False
+    except:
+        createWriteFile(item)
+        checksames(item)
+
+def appendItemQuit(lista = [""]):
+    try:
+        if path.getsize(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt'):
+            for item in lista:
+                if checksames(item):
+                    writeFile(str(item))
+        else:
+            for item in lista:
+                if checksames(item):
+                    createWriteFile(str(item))
+    except:
+        for item in lista:
+            if checksames(item):
+                createWriteFile(str(item))
+   
+    quit()
+
