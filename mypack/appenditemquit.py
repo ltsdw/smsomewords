4a098559c9b88c84a428dccd7c8946a1e345c086
ltsdw <alandms13@gmail.com>
2020-07-08 02:04:23 -0300

    ...

--- mypack/appenditemquit.py
+++ mypack/appenditemquit.py
@@ -12,7 +12,7 @@ def writeFile(item):
     f.write(item + '\n')
     f.close()
 
-def checksames(item):
+def checkSames(item):
     try:
         with open(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt') as f:
             if item not in f.read() and item != "":
@@ -26,15 +26,15 @@ def appendItemQuit(lista = [""]):
     try:
         if path.getsize(environ['DESKTOP_DIR'] + '/estudos/japonês/リスト.txt'):
             for item in lista:
-                if checksames(item):
+                if checkSames(item):
                     writeFile(str(item))
         else:
             for item in lista:
-                if checksames(item):
+                if checkSames(item):
                     createWriteFile(str(item))
     except:
         for item in lista:
-            if checksames(item):
+            if checkSames(item):
                 createWriteFile(str(item))
    
     quit()
